52d33b2b2e23ae9a8368eb171b58d8d9c576752e
DESKTOP-RBUH8AG\woo <anmy121212@gmail.com>
2023-04-09 22:38:04 +0900

    [create] ContentResolverPro

---
+++ app/src/main/java/com/example/contentresolver/MusicAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.contentresolver
+
+import android.media.MediaPlayer
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contentresolver.databinding.ItemLayoutBinding
+import java.text.SimpleDateFormat
+
+class MusicAdapter : RecyclerView.Adapter<MusicAdapter.Holder>(){
+    val musicList = mutableListOf<Music>()
+    var mediaPlayer:MediaPlayer? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding : ItemLayoutBinding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
+    }
+    override fun getItemCount(): Int {
+       return musicList.size
+    }
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val music = musicList[position]
+        holder.setMusic(music)
+    }
+
+inner class Holder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+    var musicUri: Uri? = null
+    init {
+        binding.root.setOnClickListener {
+            if(mediaPlayer != null){
+                mediaPlayer?.release()
+                mediaPlayer = null
+            }
+            mediaPlayer = MediaPlayer.create(itemView.context, musicUri)
+            mediaPlayer?.start()
+        }
+        binding.root.setOnLongClickListener {
+            musicList.removeAt(position)
+            notifyDataSetChanged()
+            true
+        }
+    }
+    fun setMusic(music: Music){
+        musicUri = music.getMusicUri()
+        binding.imageAlbum.setImageURI(music.getAlbumUri())
+        binding.textArtist.text = music.artist
+        binding.textTitle.text = music.title
+        val sdf = SimpleDateFormat("mm:ss")
+        binding.textDuration.text = sdf.format(music.duration)
+        }
+    }
+}
